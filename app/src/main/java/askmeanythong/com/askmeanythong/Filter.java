6c3b869663d932e0865cfbe653a31b9535deaea4
Sharafat Shaikh <sharafatullah@users.noreply.github.com>
2017-03-03 23:11:18 +0530

    new

--- app/src/main/java/askmeanythong/com/askmeanythong/Filter.java
+++ app/src/main/java/askmeanythong/com/askmeanythong/Filter.java
@@ -9,5 +9,7 @@ public class Filter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+        
+        //change here
     }
 }
